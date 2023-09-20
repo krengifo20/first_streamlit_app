518fb6611d88277d2872160ae8ae3c91d8e15462
krengifo20 <145112571+krengifo20@users.noreply.github.com>
2023-09-20 16:39:09 +0200

    Update streamlit_app.py
    Add more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title('Hi, greetings from VSCode')
 streamlit.header('Welcome to Streamlit')
 streamlit.text('Shall we have a drink?')
+streamlit.text('Or shall we have some food?')
+streamlit.text('Choose one: 🥣 🥗 🐔 🥑🍞')
