4bbf97949b7923941d5bb4fe5c806cccf4a688c0
krengifo20 <145112571+krengifo20@users.noreply.github.com>
2023-09-21 17:06:30 +0200

    Update streamlit_app.py
    Add snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,10 @@ if fruit_choice != '':
     streamlit.dataframe(fruityvice_normalized)
 else:
     streamlit.write('Please, enter a fruit name')
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
