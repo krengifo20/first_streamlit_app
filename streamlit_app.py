6568b07a682920e65a099a947cc2c2d817b09f42
krengifo20 <145112571+krengifo20@users.noreply.github.com>
2023-09-22 10:33:34 +0200

    Add mechanims to add fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,11 +54,23 @@ def get_fruit_list():
 
 
 
-add_fruit = streamlit.header('Show fruit list')
+streamlit.header('Show fruit list')
 if streamlit.button('Show fruit list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_row = get_fruit_list()
     streamlit.dataframe(my_data_row)
 
+
+def insert_row_fruit(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+        return 'Thanks for adding' + new_fruit
+
+add_fruit = streamlit.text_input('Do you want to add some fruit?')
+if add_fruit != '':
+    if streamlit.button('Add fruit'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        text_back = insert_row_fruit(add_fruit)
+        streamlit.text(text_back)
 
 streamlit.stop()
